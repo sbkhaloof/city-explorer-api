e72d9c6ae1d48a83c1f1bec747f2ff5fe1e63f9d
sbkhaloof <sehammalkawi92@gmail.com>
2021-07-08 13:13:27 +0300

    lab8

--- server.js
+++ server.js
@@ -49,22 +49,31 @@ server.use(cors());
 server.get('/test',testHandler)
  //localhost:3008/weather?cityName=amman
 server.get('/getWeatherInfo',getWeatherInfoHandler)
+// this for movies 
+//localhost:3008/movies?cityName=amman
+server.get('/movies',getMoviesHandler)
+
+
+
 // function handler 
 function testHandler(req,res){
+    
     res.send('all good');
 }
 function getWeatherInfoHandler(req,res){
+    
     let findQuery=req.query.cityName;
-    console.log(findQuery)
+    // console.log(findQuery)
 let weatherUrl=`https://api.weatherbit.io/v2.0/forecast/daily?city=${findQuery}&key=${process.env.WEATHER_API_KEY}`
 
 axios
 .get(weatherUrl)
-.then(weatherData=>{
+.then(weatherData=>
+    {
     let wData=weatherData.data.data.map(city=>{
         return new Forecast(city.valid_date,city.weather.description)
     })
-    res.send( wData)
+    res.send(wData)
    
 })
 
@@ -72,13 +81,46 @@ axios
     res.status(500).send(error)
 })
 }
-
+console.log('befor movie function')
+function getMoviesHandler(req,res){
+    console.log('in weather fun')
+    let findQuery=req.query.query;
+     console.log(findQuery,'findquery')
+    
+    // https://api.themoviedb.org/3/search/movie?api_key=87020c9c5f33ddf9ff4333a9ea79b990&query=irbid
+    let movieUrl=`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${findQuery}`;
+    console.log(movieUrl);
+    axios
+    .get(movieUrl)
+    .then(movieData=>{
+         console.log(movieData.data.results)
+        let movieInfo=movieData.data.results
+         console.log(movieInfo)
+        let moviesObj = movieInfo.map(movies => {
+            
+            return (new Movie(movies.title, movies.overview, movies.vote_average, movies.vote_count, movies.poster_path, movies.popularity, movies.release_date))
+        })
+        res.status(200).send(moviesObj)
+    }).catch(error=>{res.send(error)})
+}
+ console.log('after movie fun')
 class Forecast{
     constructor(date,description){
     this.date=date;
     this.description=description
 }}
 
+class Movie{
+    constructor(title,overview,avarge_votes,total_votes,image_url,popularity,relased_on){
+        this.title=title;
+        this.overview=overview;
+        this.avarge_votes=avarge_votes;
+        this.total_votes=total_votes;
+        this.image_url=`https://image.tmdb.org/t/p/w500/${image_url}`;
+        this.popularity=popularity;
+        this.relased_on=relased_on;
+    }
+}
 server.listen(PORT,()=>{
     console.log(`Listening on PORT ${PORT}`);
 })
